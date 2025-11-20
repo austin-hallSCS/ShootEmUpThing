c2ff1150b2fd368a1af5ca33fb7a912922182c12
Austin Hall <austin.hall@sumnerschools.org>
2025-11-20 13:19:44 -0600

    Just commenting out some stuff to get rid of console warnings

--- Assets/Scripts/GameManager/EventManager.cs
+++ Assets/Scripts/GameManager/EventManager.cs
@@ -6,16 +6,16 @@ namespace WizardGame.Managers
     public static class EventManager
     {
         //-- Core Game Stat Events --
-        public static event Action OnGamePause;
-        public static event Action OnGameResumed;
-        public static event Action OnGameOver;
+        // public static event Action OnGamePause;
+        // public static event Action OnGameResumed;
+        // public static event Action OnGameOver;
 
         //-- Player/XP Events --
         public static event Action<int> OnPlayerLevelUp;
 
         // -- Collectible Events --
         public static event Action<int> OnExperienceCollected;
-        public static event Action<float> OnHealthCollected;
+        // public static event Action<float> OnHealthCollected;
 
         // -- Combat/Enemy Events --
         public static event Action<EnemyController> OnEnemyDied;
