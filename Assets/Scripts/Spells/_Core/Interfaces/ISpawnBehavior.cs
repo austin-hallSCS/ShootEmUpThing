90bf936822817d971c9f322e5eefda12461c6966
Austin Hall <austin.hall@sumnerschools.org>
2026-01-23 13:58:18 -0600

    feat: add Execute method to ISpawnBehavior

--- Assets/Scripts/Spells/_Core/Interfaces/ISpawnBehavior.cs
+++ Assets/Scripts/Spells/_Core/Interfaces/ISpawnBehavior.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 namespace WizardGame.Spells
 {
     public interface ISpawnBehavior
     {
+        IEnumerator Execute(SpellCastContext ctx, IActiveBehavior activeBehavior);
         public void Spawn();
     }
 }
